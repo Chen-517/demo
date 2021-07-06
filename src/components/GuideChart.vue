d8a780595745a25280298769774ed29f650df896
醉花落蝶 <8723433+drunken-butterfly@user.noreply.gitee.com>
2021-07-06 17:10:05 +0800

    默认变更列表

---
+++ src/components/GuideChart.vue
@@ -0,0 +1,27 @@
+<template>
+<div id="GuideChart">
+  <a-carousel autoplay>
+    <div><img src="https://img1.baidu.com/it/u=2763268316,2865343497&fm=26&fmt=auto&gp=0.jpg" alt=""></div>
+    <div><img src="https://img2.baidu.com/it/u=2175702181,3942982460&fm=26&fmt=auto&gp=0.jpg" alt=""></div>
+    <div><img src="https://img2.baidu.com/it/u=2850830478,448407215&fm=26&fmt=auto&gp=0.jpg" alt=""></div>
+    <div><img src="https://img0.baidu.com/it/u=1247554568,3944534657&fm=26&fmt=auto&gp=0.jpg" alt=""></div>
+  </a-carousel>
+</div>
+</template>
+
+<script>
+export default {
+  name: "GuideChart"
+}
+</script>
+
+<style scoped lang="scss">
+
+#GuideChart{
+  width: 100%;
+  img{
+    width: 100%;
+    height: 300px;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,35 @@
 <template>
-  <div class="home">
+  <div id="Home">
+    <!--  顶部导航栏  -->
+    <div class="top">
+      <TopNavigation></TopNavigation>
+    </div>
+
+    <!--  导播图  -->
+    <div class="GuideChart">
+      <GuideChart></GuideChart>
+    </div>
+
+
 
   </div>
 </template>
 
 <script>
+
+import TopNavigation from "../components/TopNavigation";
+import GuideChart from "../components/GuideChart";
+
 export default {
   name: 'Home',
-  components: {
-  }
+  components: {TopNavigation, GuideChart}
 }
 </script>
+
+<style lang="scss" scoped>
+#Home {
+  width: 100%;
+  height: 100%;
+}
+
+</style>
